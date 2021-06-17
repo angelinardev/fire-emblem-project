30ca6dcc8d839583a828b9d70de6615c8b893686
Aga <jgillotti1352@gmail.com>
2021-06-16 23:29:40 -0400

    Cursor movement

---
+++ Assets/CursorMovementScript.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorMovementScript : MonoBehaviour
+{
+    int horizontal,
+        vertical;
+
+    public float blinkSpeed = 0.4f;
+
+    private void Start()
+    {
+        //runs function Blink at the start and reruns it every 0.4 seconds by default
+        InvokeRepeating("Blink", 0, blinkSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Controls();
+    }
+
+    void Blink()
+    {
+        //turns cursor on and off
+        gameObject.GetComponent<SpriteRenderer>().enabled = !gameObject.GetComponent<SpriteRenderer>().enabled;
+    }
+
+    private void FixedUpdate()
+    {
+        MovementUpdate();
+    }
+
+
+    private void Controls()
+    {
+        //sets left or right on
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            horizontal = (int)Input.GetAxisRaw("Horizontal");
+        }
+
+        //sets up or down on
+        if (Input.GetButtonDown("Vertical"))
+        {
+            vertical = (int)Input.GetAxisRaw("Vertical");
+        }
+    }
+
+    private void MovementUpdate()
+    {
+        if(horizontal != 0 || vertical != 0)
+        {
+            //turns cursor back on
+            if (!gameObject.GetComponent<SpriteRenderer>().enabled)
+            {
+                gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+
+        //moves the cursor once then stops additional movement
+        gameObject.transform.position += new Vector3(horizontal, vertical, 0);
+        horizontal = vertical = 0;
+
+        
+    }
+}
---
+++ Assets/OverworldCodes/StatsScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsScript : MonoBehaviour
+{
+    public int
+        Lvl = 1,
+        HP  = 1,
+        Str = 1,
+        Skl = 1,
+        Wpn = 1,
+        Spd = 1,
+        Lck = 1,
+        Def = 1,
+        Res = 1,
+        Mov = 1;
+
+    private int exp;
+
+    public Classes classes;
+
+    public enum Classes
+    {
+        Lord,
+        Mercenary,
+        Hero,
+        Thief,
+        Freelancer,
+        Fighter,
+        Pirate,
+        Archer,
+        Sniper,
+        Hunter,
+        Horseman,
+        Cavalier,
+        Paladin,
+        Knight,
+        General,
+        Pegasus_Knight,
+        Wyvern_Knight,
+        Ballistician,
+        Mage,
+        Curate,
+        Bishop,
+        Manakete
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
